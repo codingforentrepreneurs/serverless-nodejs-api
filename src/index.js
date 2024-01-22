be2360fe50592d97e932340ca69edd4616b147ae
CodingForEntrepreneurs <hello@teamcfe.com>
2024-01-22 16:41:21 -0700

    Using Dotenv to Load Sensitive Data in Each Stage

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res, next) => {
+  console.log(process.env.DEBUG )
   return res.status(200).json({
     message: "Hello from root!",
+    DEBUG: process.env.DEBUG === 1 ||  `${process.env.DEBUG}` === `1`,
+    DATABASE_URL: process.env.DATABASE_URL ? process.env.DATABASE_URL : 
+    'not here',
   });
 });
 
