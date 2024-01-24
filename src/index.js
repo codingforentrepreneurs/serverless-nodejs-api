8ea9c157fec9d1119f5f792e2e64fa3e326ba0e5
CodingForEntrepreneurs <hello@teamcfe.com>
2024-01-23 21:44:07 -0700

    9 - Integrete NodeJS to Neon Postgres

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 const serverless = require("serverless-http");
 const express = require("express");
+const { neon, neonConfig} = require('@neondatabase/serverless'); //commonjs
+
+
 const app = express();
 
-app.get("/", (req, res, next) => {
+
+async function dbClient(){
+  // for http connections
+  // non-poooling
+  neonConfig.fetchConnectionCache = true
+  const sql = neon(process.env.DATABASE_URL);
+  return sql
+}
+
+
+app.get("/", async (req, res, next) => {
   console.log(process.env.DEBUG )
+  const sql = await dbClient()
+  const [results] = await sql`select now();`
   return res.status(200).json({
     message: "Hello from root!",
-    DEBUG: process.env.DEBUG === 1 ||  `${process.env.DEBUG}` === `1`,
-    DATABASE_URL: process.env.DATABASE_URL ? process.env.DATABASE_URL : 
-    'not here',
+    results: results.now,
   });
 });
 
