bff167d049eabb5ec826dac846c1973a0614ce4a
CodingForEntrepreneurs <hello@teamcfe.com>
2024-01-26 14:50:17 -0700

    Updated dev test

--- src/lib/secrets.js
+++ src/lib/secrets.js
@@ -5,8 +5,6 @@ const { SSMClient,
 const AWS_REGION='us-east-2'
 const STAGE = process.env.STAGE || 'prod'
 
-console.log('example')
-
 async function getDatabaseUrl(){
     const DATABASE_URL_SSM_PARAM=`/serverless-nodejs-api/${STAGE}/database-url`
   const client = new SSMClient({region: AWS_REGION})
